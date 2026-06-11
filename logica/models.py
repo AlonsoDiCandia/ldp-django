e2fc4569bb0b0e5075acd1b2c875ba590caeab12
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2026-06-11 17:45:25 -0400

    Subiendo pokemon

--- logica/models.py
+++ logica/models.py
@@ -16,3 +16,15 @@ class Tortuga(models.Model):
     fuerza = models.IntegerField()
     def __str__(self):
         return f"{self.nombre}_{self.id}"
+    
+class Pokemon(models.Model):
+    nombre_pokemon = models.CharField(max_length=50)
+    num_pokedex = models.IntegerField()
+    tipo1 = models.CharField(max_length=50)
+    tipo2 = models.CharField(max_length=50)
+    hp = models.IntegerField()
+    ataque = models.IntegerField()
+    defensa = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_pokemon
--- scripts/ExtractPokemon.py
+++ scripts/ExtractPokemon.py
@@ -1,8 +1,7 @@
 # poblar_bd.py
 import os, sys
 import django
-import json
-
+import pandas as pd 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + '/backend'
 print(BASE_DIR)
@@ -10,9 +9,46 @@ sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
-from logica.models import Tortuga
+from logica.models import Pokemon
 
-tortugas = Tortuga.objects.all()
+Pokemon.objects.all().delete()
 
-for t in tortugas:
-    print(f"{t.id} {t}")
+excel_pokemon = BASE_DIR + '/utils/pokemon/excel_pokemon_usm.xlsx'
+
+pokemon_df = pd.read_excel(excel_pokemon)
+
+id = 1
+count = 0
+while id != 151:
+    print(pokemon_df.iloc[count]['Name'])
+
+    name = pokemon_df.iloc[count]['Name']
+    pokedex = pokemon_df.iloc[count]['#']
+    tipo1 = pokemon_df.iloc[count]['Type 1']
+    tipo2 = pokemon_df.iloc[count]['Type 2'] if pokemon_df.iloc[count]['Type 2'] is not None else ""
+    hp = pokemon_df.iloc[count]['HP']
+    attack = pokemon_df.iloc[count]['Attack']
+    defense = pokemon_df.iloc[count]['Defense']
+
+
+    pokemon, _ = Pokemon.objects.get_or_create(
+        nombre_pokemon = name,
+        num_pokedex = pokedex,
+        tipo1 = tipo1,
+        tipo2 = tipo2,
+        hp = hp,
+        ataque = attack,
+        defensa = defense
+    )
+
+    id = pokedex
+    count += 1
+
+print("Importacion terminada")
+
+for p in Pokemon.objects.all():
+    print(p)
+
+
+
+
