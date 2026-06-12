9d528c300f6007dd3eeaab1d4b6cd049aaffd9d5
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2026-06-12 10:19:49 -0400

    Agregando pokemon a admin

--- logica/admin.py
+++ logica/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from logica.models import Persona, Tortuga
+from logica.models import Persona, Tortuga, Pokemon
 
 # Register your models here.
 
 admin.site.register(Persona)
 admin.site.register(Tortuga)
+admin.site.register(Pokemon)
